4c18087cf10f091422d53e9ec0cbc47d4800fca9
Filip Madunicky <yelidric@gmail.com>
2025-10-05 14:01:49 +0200

    Adds MongoDB integration
    Integrates Mongoose to connect to a MongoDB database.
    
    It also adds functionality to retrieve and display notes from the database.

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = encodeURIComponent(process.argv[2]);
+
+const url = `mongodb+srv://fso_note_demo_user:${password}@cluster0.x6snlds.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+// const note = new Note({
+//   content:
+//     "When the code is executed, the program prints all the notes stored in the database",
+//   important: false,
+// });
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
