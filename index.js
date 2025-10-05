67de301bc51c9b182905dae2c23d3c6cd44d2769
Filip Madunicky <yelidric@gmail.com>
2025-10-05 18:41:59 +0200

    Connects to MongoDB and fetches notes
    Configures the application to connect to a MongoDB database using Mongoose.
    
    Fetches notes from the database instead of using hardcoded data, allowing for persistent storage and retrieval of note data.
    
    Adds `.env` to `.gitignore` to prevent accidental commit of environment variables.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const Note = require("./models/note");
 
-app.use(express.json());
+const app = express();
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -11,39 +12,18 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(express.json());
 app.use(requestLogger);
-
 app.use(express.static("dist"));
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-  {
-    id: "4",
-    content: "new note",
-    important: false,
-  },
-];
 
 app.get("/", (request, response) => {
   response.send("<h1>Ahoj svet</h1>");
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
@@ -97,7 +77,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
