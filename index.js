ea45d1a3a6b2c358fa0a79a0c4a3a598e65bfc0f
Filip Madunicky <yelidric@gmail.com>
2025-10-07 11:27:29 +0200

    Improves error handling and note validation
    Enhances error handling by adding validation error handling and implements content validation for notes, ensuring a minimum length of 5 characters.
    
    This change also passes the error to the next middleware in the post request handler.

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -56,7 +58,7 @@ app.delete("/api/notes/:id", (request, response) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (!body.content) {
@@ -70,9 +72,12 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
