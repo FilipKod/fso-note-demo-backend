2676012457ddca0a763f9a54a5691ea7f21bcf9d
Filip Madunicky <yelidric@gmail.com>
2025-10-12 17:53:07 +0200

    Configures environment and adds test
    Sets up different environments using `dotenv` for development, test, and production.
    
    Adds a test to check if notes are returned as JSON and ensures all notes are returned and contain specific content.

--- mongo.js
+++ mongo.js
@@ -1,17 +1,9 @@
+const config = require('./utils/config')
 const mongoose = require('mongoose')
-
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-const password = encodeURIComponent(process.argv[2])
-
-const url = `mongodb+srv://fso_note_demo_user:${password}@cluster0.x6snlds.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(url)
+mongoose.connect(config.MONGODB_URI)
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -20,20 +12,20 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//   content:
-//     "When the code is executed, the program prints all the notes stored in the database",
-//   important: false,
-// });
+const note = new Note({
+  content:
+    'second note in test db',
+  important: false,
+})
 
-// note.save().then((result) => {
-//   console.log("note saved!");
-//   mongoose.connection.close();
-// });
-
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note)
-  })
+note.save().then(() => {
+  console.log('note saved!')
   mongoose.connection.close()
 })
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
