c7cfb4dc1449eddb167008c4f14adf0abd33eb50
Filip Madunicky <yelidric@gmail.com>
2025-10-13 16:04:45 +0200

    Refactors note retrieval to use async/await
    Simplifies the note retrieval logic in the controller by
    using async/await for better readability.
    
    Adds setup for tests, initializing the database with
    some notes before each test. This ensures consistent
    test conditions and improves the reliability of the
    tests. Now the tests verify that notes are returned in JSON
    format, and that the correct number of notes are returned.

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,29 +1,49 @@
 const assert = require('node:assert')
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Note = require('../models/note')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true
+  }
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+test.only('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
-    .expect('Content-type', /aplication\/json/)
+    .expect('Content-type', /application\/json/)
 })
 
-test('all notes are returned', async () => {
+test.only('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
 
   const contents = response.body.map(e => e.content)
-  assert(contents.includes('second note in test db'))
+  assert(contents.includes('HTML is easy'))
 })
 
 after(async () => {
